373a5b09f872f716c50132e9378a032ac92f9eed
Jose Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-02-19 18:45:03 +0000

    Revert 1 commits
    ad9617b 'neovim:'

--- .config/nvim/lua/config/core/remap.lua
+++ .config/nvim/lua/config/core/remap.lua
@@ -1,6 +1,3 @@
--- This one in case I am using oil.nvim
--- vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
--- This one is in case I am not using oil.nvim
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
