b006ce4582424ff9d8d1a5e589147095527c95ab
Jose Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-02-20 10:56:02 +0000

    neovim: Added netrw plugin.

--- .config/nvim/lua/config/core/remap.lua
+++ .config/nvim/lua/config/core/remap.lua
@@ -1,4 +1,7 @@
+-- This one is in case I am not using oil.nvim
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- This one is in case I am using oil.nvim
+-- vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
