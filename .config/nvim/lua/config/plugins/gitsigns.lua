85bb985d55e650a1aadaa1e68b2410a6a7af2bb6
Jose Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-04-09 19:10:44 +0100

    misc changes.

--- .config/nvim/lua/config/plugins/gitsigns.lua
+++ .config/nvim/lua/config/plugins/gitsigns.lua
@@ -3,43 +3,16 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
         require("gitsigns").setup({
-            signs     = {
+            signs = {
                 add = { text = "+" },
                 change = { text = "~" },
                 delete = { text = "_" },
                 topdelete = { text = "‾" },
                 changedelete = { text = "~" },
             },
-            numhl     = true, -- Toggle with `:Gitsigns toggle_numhl`
-            on_attach = function(bufnr)
-                vim.keymap.set(
-                    "n",
-                    "<leader>hp",
-                    require("gitsigns").preview_hunk,
-                    { buffer = bufnr, desc = "Preview git hunk" }
-                )
-
-                -- don't override the built-in and fugitive keymaps
-                local gs = package.loaded.gitsigns
-                vim.keymap.set({ "n", "v" }, "]c", function()
-                    if vim.wo.diff then
-                        return "]c"
-                    end
-                    vim.schedule(function()
-                        gs.next_hunk()
-                    end)
-                    return "<Ignore>"
-                end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
-                vim.keymap.set({ "n", "v" }, "[c", function()
-                    if vim.wo.diff then
-                        return "[c"
-                    end
-                    vim.schedule(function()
-                        gs.prev_hunk()
-                    end)
-                    return "<Ignore>"
-                end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
-            end,
+            numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+            vim.keymap.set("n", "<leader>hp", require("gitsigns").preview_hunk, { desc = "Preview git hunk" }
+            )
         })
     end,
 }
