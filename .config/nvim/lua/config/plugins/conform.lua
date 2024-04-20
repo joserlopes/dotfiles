ef5013941686395c1cda0927c760c2cf5b5f7e5f
Jose Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-04-20 10:06:34 +0100

    nvim: disable format on save.

--- .config/nvim/lua/config/plugins/conform.lua
+++ .config/nvim/lua/config/plugins/conform.lua
@@ -29,11 +29,11 @@ return {
                 scss = { { "prettierd", "prettier" } },
             },
 
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                lsp_fallback = true,
-            },
+            -- format_on_save = {
+            --     -- These options will be passed to conform.format()
+            --     timeout_ms = 500,
+            --     lsp_fallback = true,
+            -- },
         })
 
         vim.keymap.set({ "n", "v" }, "<leader>l", function()
