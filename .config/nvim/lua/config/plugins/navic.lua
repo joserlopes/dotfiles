832738f04eb9318d949785025c722a7a66b1c423
Jose Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-04-26 10:06:07 +0100

    nvim: Add possible navic config file.

---
+++ .config/nvim/lua/config/plugins/navic.lua
@@ -0,0 +1,18 @@
+return {
+    --[[ "SmiteshP/nvim-navic",
+    dependencies = {
+        "LunarVim/breadcrumbs.nvim",
+    },
+    config = function()
+        require("nvim-navic").setup {
+            highlight = true,
+            lsp = {
+                auto_attach = true,
+            },
+            click = true,
+            depth_limit = 0,
+            depth_limit_indicator = "..",
+        }
+        require("breadcrumbs").setup()
+    end ]]
+}
