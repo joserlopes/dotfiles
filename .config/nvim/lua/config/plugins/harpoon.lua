be63ab71aeea5a495f05c6cfe3a98ebf86df61b9
Jose Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-05-03 09:42:32 +0100

    nvim: misc changes.

--- .config/nvim/lua/config/plugins/harpoon.lua
+++ .config/nvim/lua/config/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
 
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon add file" })
 
         vim.keymap.set("n", "<C-s>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-w>", function() harpoon:list():select(2) end)
