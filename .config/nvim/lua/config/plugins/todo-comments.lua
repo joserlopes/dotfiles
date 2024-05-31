54de765428753bbb47d1ca755419c17408f59147
Jose Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-05-31 17:54:05 +0100

    neovim: Todo comments jump keymaps.

--- .config/nvim/lua/config/plugins/todo-comments.lua
+++ .config/nvim/lua/config/plugins/todo-comments.lua
@@ -3,6 +3,13 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
 		vim.keymap.set("n", "<leader>tdf", "<cmd>TodoTelescope<cr>", { desc = "TodoTelescope", silent = true }),
+		vim.keymap.set("n", "]t", function()
+			require("todo-comments").jump_next()
+		end, { desc = "Next todo comment" }),
+
+		vim.keymap.set("n", "[t", function()
+			require("todo-comments").jump_prev()
+		end, { desc = "Previous todo comment" }),
 	},
 	config = true,
 }
