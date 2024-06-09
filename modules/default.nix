f2c073bc609efafd3d255741ef79bbf51089b4d3
José Lopes <josersl2003@gmail.com>
2024-06-09 19:36:15 +0100

    style(): A little bit more modularized.

--- modules/default.nix
+++ modules/default.nix
@@ -1,9 +1,7 @@
 {...}: {
   imports = [
-    ./graphical/alacritty.nix
-    ./editors/neovim.nix
-    ./shell/git.nix
-    ./shell/zsh.nix
-    ./shell/tmux.nix
+    ./graphical
+    ./editors
+    ./shell
   ];
 }
