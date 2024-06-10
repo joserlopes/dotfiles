c0b398349f276baaebc08c860f831862a1f7df99
José Lopes <josersl2003@gmail.com>
2024-06-10 12:13:51 +0100

    feat(i3): Use i3 status rust as default status bar.

--- modules/graphical/i3.nix
+++ modules/graphical/i3.nix
@@ -1,8 +1,17 @@
-{...}: {
+{pkgs, ...}: {
   xsession.windowManager.i3 = {
     enable = true;
     config = {
       modifier = "Mod4";
+      bars = [
+        {
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
+        }
+      ];
     };
+  };
+
+  programs.i3status-rust = {
+    enable = true;
   };
 }
