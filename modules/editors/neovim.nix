434b655d8d3c0354ad49f11411a952a52a405179
José Lopes <josersl2003@gmail.com>
2024-06-19 17:58:41 +0000

    feat(neovim): Add harpoon plugin.

--- modules/editors/neovim.nix
+++ modules/editors/neovim.nix
@@ -505,6 +505,40 @@
         vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", {desc = "Quickfix List (Trouble)"})
       '';
     }
+    {
+      plugin = harpoon2;
+      type = "lua";
+      config = ''
+        local harpoon = require("harpoon")
+        harpoon:setup()
+
+        vim.keymap.set("n", "<C-e>", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
+
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():add()
+        end, { desc = "Harpoon add file" })
+
+        vim.keymap.set("n", "<C-s>", function()
+            harpoon:list():select(1)
+        end)
+        vim.keymap.set("n", "<C-w>", function()
+            harpoon:list():select(2)
+        end)
+        vim.keymap.set("n", "<C-t>", function()
+            harpoon:list():select(3)
+        end)
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        vim.keymap.set("n", "<C-g>", function()
+            harpoon:list():prev()
+        end)
+        vim.keymap.set("n", "<C-i>", function()
+            harpoon:list():next()
+        end)
+      '';
+    }
   ];
   gitPlugins = with pkgs.unstable.vimPlugins;
     lists.optionals git [
