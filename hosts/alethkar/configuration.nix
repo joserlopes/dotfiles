fd168c2fc97989fd7cfbd1e4a3a22259657c9929
José Lopes <josersl2003@gmail.com>
2024-06-25 10:43:35 +0100

    feat(system): Trying to use nvidia drivers.
    It isn't working...

--- hosts/alethkar/configuration.nix
+++ hosts/alethkar/configuration.nix
@@ -4,7 +4,11 @@
 # URL:    https://github.com/diogotcorreia/dotfiles
 #
 # Configuration for bacchus (PC).
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   # Bootloader.
   # boot.loader.grub.enable = true;
   # boot.loader.grub.device = "/dev/disk/by-uuid/8751-786B";
@@ -43,7 +47,10 @@
   };
 
   # Bluetooth
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  };
 
   # Don't shutdown when power button is short-pressed
   services.logind.extraConfig = ''
@@ -54,6 +61,39 @@
   virtualisation.docker.enable = true;
 
   hardware.opengl.enable = true;
+
+  # # Load nvidia driver for Xorg and Wayland
+  # services.xserver.videoDrivers = ["nvidia"];
+  #
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   # Enable this if you have graphical corruption issues or application crashes after waking
+  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+  #   # of just the bare essentials.
+  #   powerManagement.enable = false;
+  #
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
+  #
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of
+  #   # supported GPUs is at:
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+  #   # Only available from driver 515.43.04+
+  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
+  #   open = false;
+  #
+  #   # Enable the Nvidia settings menu,
+  #   # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
+  #
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   # GnuPG (GPG)
   hm.programs.gpg.enable = true;
