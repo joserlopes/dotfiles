c90d749578997e198d155b0b4c44adc98d1636d1
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-06-28 12:45:43 +0100

    feat(neovim): Add obsidian nvim plugin.

--- modules/editors/neovim.nix
+++ modules/editors/neovim.nix
@@ -576,6 +576,38 @@
           })
         '';
       }
+      {
+        plugin = obsidian-nvim;
+        type = "lua";
+        config = ''
+          require("obsidian").setup({
+          		workspaces = {
+          			{
+          				name = "Notes",
+          				path = "~/Dropbox/Notes",
+          			},
+          		},
+          		completion = {
+          			-- Set to false to disable completion.
+          			nvim_cmp = true,
+
+          			-- Trigger completion at 2 chars.
+          			min_chars = 2,
+          		},
+
+          		vim.keymap.set("n", "<leader>oo", ":ObsidianOpen<CR>", { desc = "[O]bsidian [O]pen" }),
+          		vim.keymap.set("n", "<leader>on", ":ObsidianNew<CR>", { desc = "[O]bsidian [N]ew" }),
+          		vim.keymap.set("n", "<leader>ob", ":ObsidianBacklinks<CR>", { desc = "[O]bsidian [B]acklinks" }),
+          		vim.keymap.set("n", "<leader>otd", ":ObsidianToday<CR>", { desc = "[O]bsidian [T]o[m]orrow" }),
+          		vim.keymap.set("n", "<leader>otm", ":ObsidianTomorrow<CR>", { desc = "[O]bsidian [T]o[d]ay" }),
+          		vim.keymap.set("n", "<leader>oyd", ":ObsidianYesterday<CR>", { desc = "[O]bsidian [Y]ester[d]ay" }),
+          		vim.keymap.set( "n", "<leader>owp", ":ObsidianWorkspace Project<CR>", { desc = "[O]bsidian [W]orkspace [P]roject" }),
+          		vim.keymap.set( "n", "<leader>owa", ":ObsidianWorkspace Area<CR>", { desc = "[O]bsidian [W]orkspace [A]rea" }),
+          		vim.keymap.set( "n", "<leader>owr", ":ObsidianWorkspace Resource<CR>", { desc = "[O]bsidian [W]orkspace [R]esource" }),
+          		vim.keymap.set( "n", "<leader>owc", ":ObsidianWorkspace Archive<CR>", { desc = "[O]bsidian [W]orkspace [A]rchive" }),
+		  })
+        '';
+      }
     ];
   personalPackages = with pkgs;
     lists.optionals personal [
