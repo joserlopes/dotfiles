abda624614f6a7f23447ef679e52a5c4bf723b06
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-06-30 19:34:35 +0100

    feat(network): Disable iwd.

--- hosts/alethkar/default.nix
+++ hosts/alethkar/default.nix
@@ -19,17 +19,17 @@
   # Network Manager
   # TODO move to module
   networking = {
-    wireless.iwd = {
-      enable = true;
-      settings = {
-        IPv6 = {
-          Enabled = true;
-        };
-        Settings = {
-          AutoConnect = true;
-        };
-      };
-    };
+    # wireless.iwd = {
+    #   enable = true;
+    #   settings = {
+    #     IPv6 = {
+    #       Enabled = true;
+    #     };
+    #     Settings = {
+    #       AutoConnect = true;
+    #     };
+    #   };
+    # };
     networkmanager = {
       enable = true;
       # wifi.backend = "iwd";
