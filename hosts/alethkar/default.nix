619a1f7b6fa65dafd72742a42d9f676522f48490
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-07-12 12:34:24 +0100

    fix(alethkar): Network settings.

--- hosts/alethkar/default.nix
+++ hosts/alethkar/default.nix
@@ -19,20 +19,8 @@
   # Network Manager
   # TODO move to module
   networking = {
-    # wireless.iwd = {
-    #   enable = true;
-    #   settings = {
-    #     IPv6 = {
-    #       Enabled = true;
-    #     };
-    #     Settings = {
-    #       AutoConnect = true;
-    #     };
-    #   };
-    # };
     networkmanager = {
       enable = true;
-      # wifi.backend = "iwd";
     };
   };
   usr.extraGroups = ["networkmanager"];
