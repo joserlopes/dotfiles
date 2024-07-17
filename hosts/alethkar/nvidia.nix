655a4a6630b5cebdf8d0bbe9365e397e81e99ae0
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-07-17 11:40:35 +0100

    feat(alethkar): Enable nvidia drivers.

---
+++ hosts/alethkar/nvidia.nix
@@ -0,0 +1,26 @@
+# Enable NVIDIA drivers synced with Intel ones
+{config, ...}: {
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+
+    prime = {
+      # Enable sync mode
+      sync.enable = true;
+
+      # Bus ID for both GPUs
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
