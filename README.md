535d0ecd8607eebea42ed4da4261f90ba9e478fb
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-07-18 10:28:58 +0100

    misc changes: Update README.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Dotfiles for my NixOS sytems using flakes and home-manager
 
 Old arch linux [dotfiles](https://github.com/joserlopes/dotfiles/tree/arch) 
+
+## References
+
+I was heavily inspired by the following configurations when building mine:
+
+- [diogotcorreia/dotfiles](https://github.com/diogotcorreia/dotfiles)
--- modules/graphical/gtk.nix
+++ modules/graphical/gtk.nix
@@ -11,7 +11,6 @@ in {
   options.modules.graphical.gtk.enable = mkEnableOption "gtk";
 
   config = mkIf cfg.enable {
-    # https://nix-community.github.io/home-manager/index.html#_why_do_i_get_an_error_message_about_literal_ca_desrt_dconf_literal_or_literal_dconf_service_literal
     programs.dconf.enable = true;
 
     hm.gtk = {
