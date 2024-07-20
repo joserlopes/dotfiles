af3b56f7e5a70d7e8ce1e2052fd028e8dd6d1ae2
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-07-20 16:42:06 +0100

    misc changes.

--- hosts/alethkar/nvidia.nix
+++ hosts/alethkar/nvidia.nix
@@ -1,4 +1,4 @@
-# Enable NVIDIA drivers synced with Intel ones
+# Enable Nvidia drivers synced with Intel ones
 {config, ...}: {
   hardware.opengl = {
     enable = true;
@@ -16,7 +16,7 @@
       # Enable sync mode
       sync.enable = true;
 
-      # Bus ID for both GPUs
+      # Bus ID for both Intel and Nvidia GPUs
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
