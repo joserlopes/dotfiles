b4213b5dc5b13f0b8d41a284c40f210d4ecfdc45
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-07-24 16:25:30 +0100

    feat(hyprland): Add input config and useful keymaps.

--- profiles/graphical/hyprland.nix
+++ profiles/graphical/hyprland.nix
@@ -15,6 +15,21 @@
       misc = {
         disable_hyprland_logo = true;
       };
+      input = {
+        kb_layout = "pt,us";
+        kb_options = "";
+        kb_rules = "";
+
+        touchpad = {
+          natural_scroll = true;
+        };
+
+        sensitivity = 0;
+      };
+      bind = [
+        "$mod, Return, exec, alacritty"
+        "$mod, B, exec, firefox"
+      ];
     };
   };
 }
