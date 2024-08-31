2915b0f9db21d9d659afa116237896ed4e81cb5a
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-08-31 19:06:54 +0100

    feat(nix): Add formatters to dev-tools.

--- nix/home-manager/dev/tools.nix
+++ nix/home-manager/dev/tools.nix
@@ -5,6 +5,19 @@
 
     # better fzf
     skim
+
+    # Formatters
+    nodePackages.prettier # JavaScript et al. formatter
+    prettierd # JavaScript et al. formatter as daemon
+    ruff # Python linter and formatter
+    stylua # Lua formatter
+    buf # Proto formatter
+    google-java-format # Java formatter
+    beautysh # Shell scripts formatter
+    ktlint # Kotlin formatter
+    taplo # TOML formatter
+    typstyle # Typst formatter
+    yamlfix # YAML formatter
   ];
 
   programs.lazygit.enable = true;
