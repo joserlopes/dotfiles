5e34e1500f00744a6597186721239fd6b415f642
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-08-31 18:25:32 +0100

    feat(nvim): misc changes.

--- .config/nvim/lua/config/plugins/neogit.lua
+++ .config/nvim/lua/config/plugins/neogit.lua
@@ -1,13 +1,9 @@
 return {
     "NeogitOrg/neogit",
-    --branch = "nightly",
     dependencies = {
-        "nvim-lua/plenary.nvim",   -- required
-        "sindrets/diffview.nvim",  -- optional - Diff integration
-
-        "nvim-telescope/telescope.nvim", -- optional
+        "nvim-lua/plenary.nvim",  -- required
+        "sindrets/diffview.nvim", -- optional - Diff integration
     },
     config = true,
-
     keys = { { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit", silent = true } },
 }
