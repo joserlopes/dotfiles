d59d6fd80fecc55b0ba0aa7b3141b96df9b7768d
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-08-31 19:06:41 +0100

    feat(nvim): Update conform's config.

--- .config/nvim/lua/config/plugins/conform.lua
+++ .config/nvim/lua/config/plugins/conform.lua
@@ -1,48 +1,51 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-        conform.setup({
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            },
-            formatters_by_ft = {
-                lua = { "stylua" },
-                svelte = { "prettierd", "prettier" },
-                nix = { "alejandra" },
-                javascript = { "prettierd", "prettier" },
-                typescript = { "prettierd", "prettier" },
-                javascriptreact = { "prettierd", "prettier" },
-                typescriptreact = { "prettierd", "prettier" },
-                json = { "prettierd", "prettier" },
-                graphql = { "prettierd", "prettier" },
-                java = { "google-java-format" },
-                kotlin = { "ktlint" },
-                ruby = { "standardrb" },
-                markdown = { "prettierd", "prettier" },
-                erb = { "prettierd", "prettier" },
-                html = { "prettierd", "prettier" },
-                bash = { "beautysh" },
-                proto = { "buf" },
-                python = { "black" },
-                rust = { "rustfmt" },
-                go = { "gofmt" },
-                yaml = { "yamlfix" },
-                toml = { "taplo" },
-                typst = { "typstyle" },
-                css = { "prettierd", "prettier" },
-                scss = { "prettierd", "prettier" },
-            },
-        })
+		conform.setup({
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				nix = { "alejandra" },
+				svelte = { "prettierd", "prettier", stop_after_first = true },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+				graphql = { "prettierd", "prettier", stop_after_first = true },
+				java = { "google-java-format" },
+				kotlin = { "ktlint" },
+				ruby = { "standardrb" },
+				markdown = { "prettierd", "prettier", stop_after_first = true },
+				erb = { "prettierd", "prettier", stop_after_first = true },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				bash = { "beautysh" },
+				proto = { "buf" },
+				python = {
+					-- To fix lint errors "ruff_fix",
+					"ruff_format",
+				},
+				rust = { "rustfmt" },
+				go = { "gofmt" },
+				yaml = { "yamlfix" },
+				toml = { "taplo" },
+				typst = { "typstyle" },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				scss = { "prettierd", "prettier", stop_after_first = true },
+			},
+		})
 
-        vim.keymap.set({ "n", "v" }, "<leader>l", function()
-            conform.format({
-                lsp_format = "fallback",
-                quiet = true,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+		vim.keymap.set({ "n", "v" }, "<leader>l", function()
+			conform.format({
+				lsp_format = "fallback",
+				quiet = true,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
