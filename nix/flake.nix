9f50a8b210fcbcdb4cf3d20737dc3a5a9d3d9465
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-09-02 18:04:21 +0100

    feat(pkgs): Add overlays option and unstable as a set of pkgs.

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,22 +2,30 @@
   description = "My Pop_OS Nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    nixpkgs,
-    home-manager,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs @ {...}: let
+    inherit (lib.my) mkOverlays mkPkgs;
+
+    lib = inputs.nixpkgs.lib.extend (self: _super:
+      import ./lib {
+        inherit inputs pkgs;
+        lib = self;
+      });
+    overlays =
+      (mkOverlays ./overlays)
+      // {
+        extraPkgs = _self: _super: {system = "x86_64-linux";};
+      };
+    pkgs = mkPkgs overlays;
   in {
     homeConfigurations = {
-      jrl = home-manager.lib.homeManagerConfiguration {
+      jrl = inputs.home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
           ./home-manager/home.nix
