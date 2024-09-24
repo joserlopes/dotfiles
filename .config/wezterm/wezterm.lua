8aff82ce015e576fc51e3dddad9fa83f062a3b0f
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-09-24 11:22:41 +0100

    feat(terminal): Default to wezterm.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,16 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local mux = wezterm.mux
 
-config.font = wezterm.font("0xProto")
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
+
+config.font = wezterm.font("0xProto", { weight = "Regular" })
 config.font_size = 16
-config.color_scheme = "Everforest Dark (Gogh)"
-config.window_background_opacity = 0.8
+config.color_scheme = "Black Metal (Gorgoroth) (base16)"
+config.window_background_opacity = 1
+config.enable_tab_bar = false
 
 return config
