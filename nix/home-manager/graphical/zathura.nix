7d2559c84b00e4eb0b02fcf7ff1ab9c50fd75813
José Lopes <jose.r.lopes@tecnico.ulisboa.pt>
2024-12-06 17:57:28 +0000

    feat(zathura): Open files in fullscreen.

--- nix/home-manager/graphical/zathura.nix
+++ nix/home-manager/graphical/zathura.nix
@@ -5,6 +5,8 @@
     options = {
       selection-clipboard = "clipboard";
       sandbox = "none"; # fix links not opening on browser
+      window-height = 3000;
+      window-width = 3000;
     };
   };
 }
