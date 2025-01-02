619a21d794348a030a0452965ff1b6872d3db39f
José Lopes <josersl2003@gmail.com>
2025-01-02 16:17:50 +0000

    feat(fish): Add more aliases.

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -1,3 +1,3 @@
 alias c="clear"
 alias v="nvim"
-alias l="eza -l -L=1 --git --icons --color=always --group-directories-first"
+alias l="eza -la -L=1 --git --icons --color=always --group-directories-first"
