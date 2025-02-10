a3f6f54bd5eac0b3ccbc2e4e90c2a80fce9d4655
José Lopes <josersl2003@gmail.com>
2025-02-10 20:21:19 +0000

    feat(nvim): Add tailwind tools.

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -11,6 +11,19 @@ return {
 
 	"runoshun/vim-alloy",
 
+	-- tailwind-tools.lua
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		name = "tailwind-tools",
+		build = ":UpdateRemotePlugins",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- optional
+			"neovim/nvim-lspconfig", -- optional
+		},
+		opts = {}, -- your configuration
+	},
+
 	{
 		"nvzone/showkeys",
 		cmd = "ShowkeysToggle",
