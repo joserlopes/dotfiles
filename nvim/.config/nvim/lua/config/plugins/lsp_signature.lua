8c3471d13704a99b0f788df013c14abebd4d5a0d
José Lopes <josersl2003@gmail.com>
2025-04-19 18:01:45 +0100

    feat(nvim): lsp_signature remove hint.

--- nvim/.config/nvim/lua/config/plugins/lsp_signature.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp_signature.lua
@@ -2,10 +2,6 @@ return {
 	"ray-x/lsp_signature.nvim",
 	event = "VeryLazy",
 	opts = {
-		hint_prefix = {
-			above = "↙ ", -- when the hint is on the line above the current line
-			current = "← ", -- when the hint is on the same line
-			below = "↖ ", -- when the hint is on the line below the current line
-		},
+		hint_enable = false,
 	},
 }
