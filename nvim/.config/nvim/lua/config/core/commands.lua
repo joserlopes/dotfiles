e3eb9c119800a716f463c1dfe1ef7a07b8254456
José Lopes <josersl2003@gmail.com>
2025-04-23 11:08:29 +0100

    feat(nvim): Typst command automatically closes zathura.

--- nvim/.config/nvim/lua/config/core/commands.lua
+++ nvim/.config/nvim/lua/config/core/commands.lua
@@ -1,11 +1,22 @@
 -- A sort of typst preview but with pdf
--- See here: https://myriad-dreamin.github.io/tinymist/frontend/neovim.html#label-Live%20Preview
-vim.api.nvim_create_user_command("OpenPdf", function()
+-- See here: https://myriad-dreamin.github.io/tinymist/frontend/neovim.html#label-Live%20Preview for inspiration
+vim.api.nvim_create_user_command("TypstWatch", function()
 	local filepath = vim.api.nvim_buf_get_name(0)
 	if filepath:match("%.typ$") then
 		local pdf_path = filepath:gsub("%.typ$", ".pdf")
-		vim.system({ "zathura", pdf_path })
+		-- Start zathura and capture the process handle
+		local handle = vim.system({ "zathura", pdf_path }, { detach = true })
+
+		-- Create autocmd to kill zathura when neovim exits
+		vim.api.nvim_create_autocmd("VimLeave", {
+			callback = function()
+				if handle and handle.pid then
+					vim.system({ "kill", tostring(handle.pid) })
+				end
+			end,
+			group = vim.api.nvim_create_augroup("ZathuraKiller", { clear = true }),
+		})
 	end
 end, {})
 
-vim.keymap.set("n", "<leader>tw", "<CMD>OpenPdf<CR>", { desc = "Typst Watch" })
+vim.keymap.set("n", "<leader>tw", "<CMD>TypstWatch<CR>", { desc = "Typst Watch" })
