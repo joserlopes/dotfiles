107cc65518dfffa442a729d3e005659c7ad98b4c
José Lopes <josersl2003@gmail.com>
2025-04-30 14:24:56 +0100

    just some misc changes.

--- nvim/.config/nvim/lua/config/plugins/cmp.lua
+++ nvim/.config/nvim/lua/config/plugins/cmp.lua
@@ -28,7 +28,10 @@ return {
 		completion = {
 			documentation = {
 				auto_show = true,
-				auto_show_delay_ms = 100,
+				auto_show_delay_ms = 0,
+			},
+			menu = {
+				scrollbar = false,
 			},
 		},
 
