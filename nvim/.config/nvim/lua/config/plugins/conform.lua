7ddaaeb16026370250cbee7cfe04516eb20c872f
José Lopes <josersl2003@gmail.com>
2025-05-03 13:19:29 +0100

    feat(nvim): Add text wrap to typstyle.

--- nvim/.config/nvim/lua/config/plugins/conform.lua
+++ nvim/.config/nvim/lua/config/plugins/conform.lua
@@ -4,6 +4,12 @@ return {
 	config = function()
 		local conform = require("conform")
 
+		-- Config typstyle to wrap text on column 80
+		conform.formatters.typstyle = {
+			command = "typstyle",
+			args = "--wrap-text",
+		}
+
 		conform.setup({
 			format_on_save = function(bufnr)
 				-- Disable with a global or buffer-local variable
