534cf127d0055a41dd7db752bdc1930f39a407f3
José Lopes <josersl2003@gmail.com>
2025-05-03 13:19:08 +0100

    fix(nvim): Grapple keybinds no longer interfere with present ones.

--- nvim/.config/nvim/lua/config/plugins/grapple.lua
+++ nvim/.config/nvim/lua/config/plugins/grapple.lua
@@ -7,10 +7,11 @@ return {
 		{ "<leader>a", "<cmd>Grapple toggle<cr>", desc = "Tag a file" },
 		{ "<c-e>", "<cmd>Grapple toggle_tags<cr>", desc = "Toggle tags menu" },
 
-		{ "<c-a>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
-		{ "<c-s>", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
-		{ "<c-j>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
-		{ "<c-k>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+		{ "<c-s>", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+		-- For some reason ç counts as <c-ç> but <c-ç> does nothing...
+		{ "ç", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+		{ "<c-w>", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+		{ "<c-y>", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
 
 		{ "<c-s-n>", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
 		{ "<c-s-p>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
