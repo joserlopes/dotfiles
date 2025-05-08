45da7d5eaad73ab257115b42091daa35a3cb4083
José Lopes <josersl2003@gmail.com>
2025-05-08 09:11:26 +0100

    feat(nvim): Move lsp on_attach function to utilities. Update Mason.

--- nvim/.config/nvim/lua/config/plugins/crates.lua
+++ nvim/.config/nvim/lua/config/plugins/crates.lua
@@ -1,54 +1,8 @@
-local on_attach = function(client, bufnr)
-	-- In this case, we create a function that lets us more easily define mappings specific
-	-- for LSP related items. It sets the mode, buffer and description for us each time.
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = "LSP: " .. desc
-		end
-
-		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-	end
-
-	nmap("<leader>vrn", vim.lsp.buf.rename, "[R]e[n]ame")
-	nmap("<leader>vca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-	nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-	nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-	nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-	-- See `:help K` for why this keymap
-	vim.keymap.set("i", "<C-h>", function()
-		vim.lsp.buf.signature_help()
-	end, { desc = "LSP: Signature Help" })
-
-	-- Lesser used LSP functionality
-	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-	nmap("<leader>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, "[W]orkspace [L]ist Folders")
-	nmap("<leader>vd", function()
-		vim.diagnostic.open_float()
-	end)
-
-	-- Create a command `:Format` local to the LSP buffer
-	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-		vim.lsp.buf.format()
-	end, { desc = "Format current buffer with LSP" })
-
-	nmap("<leader>hl", function()
-		vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-	end, "Toggle [I]nlay [H]ints")
-end
-
 return {
 	"saecki/crates.nvim",
 	tag = "stable",
 	config = function()
+		local on_attach = require("nice_utils").on_attach
 		require("crates").setup({
 			lsp = {
 				enabled = true,
@@ -56,14 +10,6 @@ return {
 				actions = true,
 				completion = true,
 				hover = true,
-			},
-			completion = {
-				cmp = {
-					enabled = true,
-				},
-				crates = {
-					enabled = true,
-				},
 			},
 		})
 	end,
--- nvim/.config/nvim/lua/nice_utils/init.lua
+++ nvim/.config/nvim/lua/nice_utils/init.lua
@@ -37,4 +37,50 @@ function utils.center_in(outer, inner)
 	return (outer - inner) / 2
 end
 
+-- on_attach for LSP
+function utils.on_attach(client, bufnr)
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
+
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	nmap("<leader>vrn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>vca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("<leader>vrf", vim.lsp.buf.references, "[G]oto [R]eferences")
+	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", vim.lsp.buf.document_symbol, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", vim.lsp.buf.workspace_symbol, "[W]orkspace [S]ymbols")
+
+	-- See `:help K` for why this keymap
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, { desc = "LSP: Signature Help" })
+
+	-- Lesser used LSP functionality
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
+	nmap("<leader>vd", function()
+		vim.diagnostic.open_float()
+	end)
+
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, { desc = "Format current buffer with LSP" })
+
+	nmap("<leader>hl", function()
+		vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+	end, "Toggle [I]nlay [H]ints")
+end
+
 return utils
