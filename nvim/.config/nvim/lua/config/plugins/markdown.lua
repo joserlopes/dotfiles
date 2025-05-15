27421ab16fd1e5130d1878fa6ca1a7f4920cb96d
José Lopes <josersl2003@gmail.com>
2025-05-15 12:22:40 +0100

    cleanup.

--- nvim/.config/nvim/lua/config/plugins/markdown.lua
+++ nvim/.config/nvim/lua/config/plugins/markdown.lua
@@ -2,5 +2,4 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	opts = {},
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
 }
