c6b71fdd384675c7c2a3d8cff571dff79cc4ceab
José Lopes <josersl2003@gmail.com>
2025-09-11 15:56:36 +0100

    misc changes.

--- nvim/.config/nvim/lua/config/plugins/ts.lua
+++ nvim/.config/nvim/lua/config/plugins/ts.lua
@@ -1,5 +1,5 @@
 return {
-	"pmizio/typescript-tools.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {},
+	-- "pmizio/typescript-tools.nvim",
+	-- dependencies = { "nvim-lua/plenary.nvim" },
+	-- opts = {},
 }
