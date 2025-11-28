8151350f61b810fc66999d7ebeb8d9af2fa89f37
José Lopes <josersl2003@gmail.com>
2025-11-28 11:13:55 +0000

    Change font to MonaspiceKr NF and terminal back to alacritty.

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -2,4 +2,16 @@ return {
 	"christoomey/vim-tmux-navigator",
 	"hiphish/rainbow-delimiters.nvim",
 	"mfussenegger/nvim-jdtls",
+	{
+		"saghen/blink.indent",
+		opts = {},
+	},
+
+	"vim-scripts/promela.vim",
+
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		opts = {},
+	},
 }
