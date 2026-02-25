260e64e298b17d76f7c4f76330920b9510ec5ef2
José Lopes <josersl2003@gmail.com>
2026-02-25 10:57:44 +0000

    Back to everforest.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,10 +13,16 @@ function fish_user_key_bindings
 	end
 end
 
+# fish_add_path -a ~/.local/bin
+# fish_add_path -a ~/go/bin/
+# fish_add_path -a ~/.juliaup/bin
+
 # Add ~/.local/bin to PATH
-fish_add_path -a ~/.local/bin
+set -gx PATH $HOME/.local/bin $PATH
 # Add go
-fish_add_path -a ~/go/bin/
+set -gx PATH $HOME/go/bin $PATH
+# Add juliaup 
+set -gx PATH $HOME/.juliaup/bin $PATH
 
 zoxide init --cmd cd fish | source
 starship init fish | source
@@ -24,3 +30,7 @@ starship init fish | source
 set fish_greeting
 set -gx EDITOR nvim
 set -gx VISUAL nvim
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/jrl/.ghcup/bin $PATH # ghcup-env
+
+eval (opam env --switch=default)
--- nvim/.config/nvim/lua/config/plugins/colors.lua
+++ nvim/.config/nvim/lua/config/plugins/colors.lua
@@ -1,20 +1,42 @@
 return {
-	{
-		"thesimonho/kanagawa-paper.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("kanagawa-paper").setup({
-				overrides = function(colors)
-					return {
-						["@comment.documentation"] = { fg = colors.palette.waveAqua2, italic = true },
-						["@comment"] = { fg = colors.palette.canvasAsh1, italic = true },
-					}
-				end,
-			})
-		vim.cmd.colorscheme("kanagawa-paper")
-		end,
-	},
+
+    {
+        "neanias/everforest-nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local everforest = require("everforest")
+            everforest.setup({
+                background = "hard",
+                -- This is for enabling transparent background
+                transparent_background_level = 1,
+                italics = true,
+
+                on_highlights = function(hl, palette)
+                    hl["@comment.documentation.rust"] = { fg = palette.aqua, italic = true }
+                    hl["@lsp.type.comment.rust"] = { fg = palette.aqua, italic = true }
+                    hl.TSComment = { fg = "#928374", italic = true }
+                end,
+            })
+            vim.cmd.colorscheme("everforest")
+        end,
+    },
+
+    {
+        "thesimonho/kanagawa-paper.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("kanagawa-paper").setup({
+                overrides = function(colors)
+                    return {
+                        ["@comment.documentation"] = { fg = colors.palette.waveAqua2, italic = true },
+                        ["@comment"] = { fg = colors.palette.canvasAsh1, italic = true },
+                    }
+                end,
+            })
+            -- vim.cmd.colorscheme("kanagawa-paper")
+        end,
+    },
 
 }
-
