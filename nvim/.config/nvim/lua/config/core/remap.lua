77242ad8d71bb7d2ac2466d7045362677ff0a722
José Lopes <josersl2003@gmail.com>
2026-03-04 11:11:48 +0000

    feat(nvim): Add new replace remap.

--- nvim/.config/nvim/lua/config/core/remap.lua
+++ nvim/.config/nvim/lua/config/core/remap.lua
@@ -42,6 +42,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>e", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("v", "<leader>r", [[y:%s/<C-r>"//gI<Left><Left><Left>]])
 
 -- avoid typing W instead of w to save the file
 vim.cmd("cabbrev W w")
